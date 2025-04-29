be1089e66a5f8b5e3e68fe76edf98814cf149dcc
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-04-29 11:48:03 +0200

    verify email and password

--- config.php
+++ config.php
@@ -1,7 +1,19 @@
 <?php
-if(isset($_POST['email']) && isset($_POST['email'])) {
-    echo " Bienvenue" ;
-}else {
-    echo " Veuillez rentrer vos  identifiants";
-}
+if(isset($_POST['email']) && isset($_POST['mdp'])) {
+    $email = $_POST['email'];
+    $mdp = $_POST['mdp'];
+    $nom_serveur ="localhost";
+    $utilisateur = "root";
+    $mot_de_passe ="";
+    $nom_base_données ="form";
+    $con = mysqli_connect($nom_serveur, $utilisateur, $mot_de_passe, $nom_base_données);
+    $req = mysqli_query($con, "SELECT * FROM utilisateurs WHERE email='$email' AND mdp = '$mdp'");
+    $num_ligne = mysqli_num_rows($req);
+    if($num_ligne > 0){
+        header("Location: bienvenue.php");
+        exit();
+    } else {
+        echo "Adresse Mail ou Mot de passe incorrect !";
+    }
+    }
 ?>
