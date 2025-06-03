0a64700605ec1119d995175dc69278a0141df3fa
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-06-03 18:10:35 +0200

    add red error message

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
+<?php
+    if(isset($_POST['bouton-valider'])){
+    if(isset($_POST['email']) && isset($_POST['mdp'])) {
+    $email = $_POST['email'];
+    $mdp = $_POST['mdp'];
+    $erreur = "" ;
+    $nom_serveur ="localhost";
+    $utilisateur = "root";
+    $mot_de_passe ="";
+    $nom_base_données ="form";
+    $con = mysqli_connect($nom_serveur, $utilisateur, $mot_de_passe, $nom_base_données);
+    $req = mysqli_query($con, "SELECT * FROM utilisateurs WHERE email='$email' AND mdp = '$mdp'");
+    $num_ligne = mysqli_num_rows($req);
+    if($num_ligne > 0){
+        header("Location: bienvenue.php");
+        exit();
+    } else {
+        $erreur = "Adresse Mail ou Mot de passe incorrect !";
+    }
+    }
+    }
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -10,12 +32,17 @@
 <body>
     <section>
     <h1>Connexion</h1>
-    <form action="config.php" method="POST">
+    <?php
+    if(isset($erreur)){
+            echo "<p class= 'Erreur' >".$erreur."</p>" ;
+    }
+    ?>
+    <form action="" method="POST">
         <label>Email</label>
         <input type="text" name="email">
         <label>Mot de passe</label>
         <input type="password" name="mdp">
-        <input type="submit" value="Valider">  
+        <input type="submit" value="Valider" name="bouton-valider">  
     </form>
     </section>
 </body>
